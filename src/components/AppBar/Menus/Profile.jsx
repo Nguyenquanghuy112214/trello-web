805bb1eb2bd41a721c75c182e2c1a9f11826d92c
QuangHuy <NguyenQuyangHuy@gmail.com>
2023-11-27 21:03:55 +0700

    app_bar_p1

---
+++ src/components/AppBar/Menus/Profile.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Check from '@mui/icons-material/Check'
+import { Avatar, IconButton, Tooltip } from '@mui/material'
+import { Logout, PersonAdd, Settings } from '@mui/icons-material'
+export default function Profile() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ p: 0 }}
+          aria-controls={open ? 'basic-menu-profile' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar sx={{ width: 30, height: 32 }} alt="QuangHuyDev" src='https://scontent.fhan2-3.fna.fbcdn.net/v/t39.30808-6/353817796_3430709953868970_374175792483119879_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=7sRCxELTJwwAX-pAd6K&_nc_ht=scontent.fhan2-3.fna&oh=00_AfDgSJSFtcQaKHGtc-jaC6JN78BScOhIIitgGWD-bfAkIA&oe=656A6D84'></Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu-profile"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-profile'
+        }}
+      >
+        <MenuItem >
+          <Avatar sx={{ width: '28px', height: '28px', mr: 2 }} /> Profile
+        </MenuItem>
+        <MenuItem >
+          <Avatar sx={{ width: '28px', height: '28px', mr: 2 }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem >
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem >
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem >
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
