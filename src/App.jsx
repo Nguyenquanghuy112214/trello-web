deff6ab05ad343e31656aa94e29f6dadb0434ef2
QuangHuy <NguyenQuyangHuy@gmail.com>
2023-11-27 09:17:10 +0700

    dark_light_p1

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,27 @@ import Button from '@mui/material/Button'
 import './App.css'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import {
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
   return (
     <div>
+      <ModeToggle />
+      <hr />
       <AccessAlarmIcon />
       <ThreeDRotation />
       <div>QuangHuydev</div>
